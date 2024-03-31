711e34eb442ba70f9223b7ef48791af42639f7c7
faishalnafi <faishalnafi50@gmail.com>
2024-03-31 12:33:25 +0700

    program pertama

---
+++ main.js
@@ -0,0 +1,10 @@
+//Ini adalah program master da dasar belajar javascript
+//Dibuat oleh faishalnafi;
+//Tahun 2024 03 31;
+
+console.log("Hello World");
+var a = 10;
+console.log(a);
+alert("Ini adalah alert");
+console.log('ini bisa menggunakan kutip satu');
+console.log('bisa begini juga = ' & a);
