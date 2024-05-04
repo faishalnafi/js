3be3455b6b52e64a34f83f5ddce0e97173ee3e85
faishalnafi <faishalnafi50@gmail.com>
2024-05-04 23:33:05 +0700

    Update: data string

--- tipedata.js
+++ tipedata.js
@@ -1,3 +1,10 @@
 //Ini adalah program master da dasar belajar javascript
 //Dibuat oleh faishalnafi;
 //Tahun 2024 05 04;
+
+//data string
+let datastring = "ucup";
+console.log(datastring);
+console.log(typeof datastring);
+
+//data number
